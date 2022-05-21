507e747db638b36fdd8a6bfcd1560d816874a09f
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-21 19:15:48 +0300

    int_to_binary.c

--- int_to_binary.c
+++ int_to_binary.c
@@ -35,3 +35,148 @@ int p_binary(va_list ap)
 	return (len);
 }
 
+/**
+ * p_udec - prints an unsigned int in decimal
+ * @ap: uint to be printed
+ * Return: number of chars printed
+ */
+
+int p_udec(va_list ap)
+{
+	unsigned int n, arr[10], len = 0;
+	int i = 0;
+
+	n = va_arg(ap, unsigned int);
+
+	if (!n)
+	{
+		i++;
+		arr[0] = 0;
+	}
+	while (n > 0)
+	{
+		arr[i] = n % 10;
+		n /= 10;
+		i++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+		_putchar(arr[i] + '0');
+		i--;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * p_oct - prints an unsigned int in octal
+ * @ap: uint to be printed
+ * Return: number of chars printed
+ */
+
+int p_oct(va_list ap)
+{
+	unsigned int n, arr[11], len = 0;
+	int i = 0;
+
+	n = va_arg(ap, unsigned int);
+
+	if (!n)
+	{
+		i++;
+		arr[0] = 0;
+	}
+	while (n > 0)
+	{
+		arr[i] = n % 8;
+		n /= 8;
+		i++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+		_putchar(arr[i] + '0');
+		i--;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * p_xhex - prints a uint in lowercase hex
+ * @ap: int to be printed
+ * Return: number of chars printed
+ */
+
+int p_xhex(va_list ap)
+{
+	unsigned int n, arr[8], len = 0;
+	int i = 0;
+
+	n = va_arg(ap, unsigned int);
+
+	if (!n)
+	{
+		i++;
+		arr[0] = 0;
+	}
+	while (n > 0)
+	{
+		arr[i] = n % 16;
+		n /= 16;
+		i++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+		if (arr[i] < 10)
+			_putchar(arr[i] + '0');
+		else
+			_putchar(arr[i] + '0' + 'a' - ':');
+		i--;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * p_Xhex - prints a uint in uppercase hex
+ * @ap: the uint to be printed
+ * Return: no. of chars printed
+ */
+
+int p_Xhex(va_list ap)
+{
+	unsigned int n, arr[8], len = 0;
+	int i = 0;
+
+	n = va_arg(ap, unsigned int);
+
+	if (!n)
+	{
+		i++;
+		arr[0] = 0;
+	}
+	while (n > 0)
+	{
+		arr[i] = n % 16;
+		n /= 16;
+		i++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+		if (arr[i] < 10)
+			_putchar(arr[i] + '0');
+		else
+			_putchar(arr[i] + '0' + 'A' - ':');
+		i--;
+		len++;
+	}
+	return (len);
+}
