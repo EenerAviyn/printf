4ce4cca302e7f23a5f6afdd20fd96d20280221a2
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-22 22:56:25 +0300

    _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,37 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - prints a character c to the stdout
- * @c: character to printed on stdout
- * Return: 1 on success or -1 on error.
+ * _puts - prints a string with newline
+ * @str: string to print
+ * Return: void
  */
 
-int _putchar(char c)
+int _puts(char *str)
 {
-	return (write(1, &c, 1));
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+
+/**
+ * _putchar - writes character c to stdout
+ * @c: character to print
+ * Return: On success 1 or -1
+ */
+
+int _putchar(int c)
+{
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
