d10e077c52b58558daea14ff7a277bb6d891cea5
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-22 21:47:30 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,49 +1,92 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * _printf - prints output
- * @format: the format string
+ * func_selector - selects a function based on corresponding specifier.
+ * @format: specifier
  *
- * Return: number of bytes printed
+ * Return: pointer to corresponding function.
  */
+static int (*func_selector(const char *format))(va_list)
+{
+	unsigned int i = 0;
+	id_f identf[] = {
+		{"c", p_char},
+		{"s", p_str},
+		{"d", p_dec},
+		{"i", p_int},
+		{"b", p_binary},
+		{"u", p_udec},
+		{"o", p_oct},
+		{"x", p_xhex},
+		{"X", p_Xhex},
+		{"S", p_S},
+		{"p", p_p},
+		{"R", p_R},
+		{"r", p_r},
+		{NULL, NULL}
+	};
+
+	while (identf[i].id != NULL)
+	{
+		if (*(identf[i].id) == *format)
+		{
+			break;
+		}
+		i++;
+	}
+	return (identf[i].f);
+}
+
+/**
+ * _printf - produces output according to a format.
+ * @format: character string.
+ *
+ * Return: the number of characters printed (excluding the null byte used to
+ * end the output to strings on success or -1 on error.
+ */
+
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
+	unsigned int i = 0, j = 0;		/* i -loop and j -counter */
+	va_list ap;				/* req in variadic functions */
+	int (*func)(va_list);			/* pointer to function */
 
+	/* check for nullity */
+	if (format == NULL)
+		return (-1);
 	va_start(ap, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = (char *)format; *p; p++)
+	char *buffer = malloc(sizeof(char) * 1024);
+	while (format[i])
 	{
-		init_params(&params, ap);
-		if (*p != '%')
+		while (format[i] != '%' && format[i])
 		{
-			sum += _putchar(*p);
+			buffer[i] = format[i];
+			i++;
+			j++;
+		}
+		if (!format[i])
+			return (j);
+		func = func_selector(&format[i + 1]);
+		if (func != NULL)
+		{
+			j += func(ap);		/* count what is printed */
+			i += 2;			/* escape '%' and identifier */
 			continue;
 		}
-		start = p;
-		p++;
-		while (get_flag(p, &params))
-		{
-			p++;
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
-			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-								 params.l_modifier || params.h_modifier ? p - 1 : 0);
+		if (!format[i + 1])
+			return (-1);
+		buffer[i] = format[i];
+		j++;
+		if (format[i + 1] == '%')
+			i += 2;
 		else
-			sum += get_print_func(p, ap, &params);
+			i++;
 	}
-	_putchar(BUF_FLUSH);
+	write(1, buffer, i)
 	va_end(ap);
-	return (sum);
+
+	return (j);
 }
