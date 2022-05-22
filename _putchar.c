2a8add9e264026e6f558a88e145b76cc13e9991d
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-22 22:04:36 +0300

    _putchar.c

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints a character c to the stdout
+ * @c: character to printed on stdout
+ * Return: 1 on success or -1 on error.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
